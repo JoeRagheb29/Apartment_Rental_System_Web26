39f50ca4ebdea88d2140680ba4ee0656f5200561
joeragheb29 <youssifragheb838@gmail.com>
2026-05-03 23:03:23 +0300

    add roles and inhance UI of register form

--- Frontend/src/Auth/Register.jsx
+++ Frontend/src/Auth/Register.jsx
@@ -3,87 +3,89 @@ import styles from './Register.module.css';
 import axios from 'axios';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import * as yup from 'yup'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://localhost:5000/',
   timeout: 1000,
 });
 
 function Register() {
+  // const [role, setRole] = useState('');
   const [isSubmitting, setSubmitting] = useState(false);
 
-  const handleSubmit = (values) => {
-    setSubmitting(true);
-    API.post('/api/auth/register', values)
-      .then(response => {
-        console.log('User registered:', response.data);
-      })
-      .catch(error => {
-        console.error('Error registering user:', error);
-      })
-      .finally(() => {
-        setSubmitting(false);
-      });
-  };
+  const formFields = [
+    { id: 'fullname', name: 'fullname', label: 'Full Name', type: 'text', required: true },
+    { id: 'email', name: 'email', label: 'Email', type: 'email', required: true },
+    { id: 'password', name: 'password', label: 'Password', type: 'password', required: true },
+    { id: 'confirmPassword', name: 'confirmPassword', label: 'Confirm Password', type: 'password', required: true },
+  ];
 
-  const handleValidate = (values) => {
-    const errors = {};
+  const YupSchema = yup.object().shape({
+    fullname: yup.string().required('Full Name is required'),
+    email: yup.string().email('Invalid email address').required('Email is required'),
+    role: yup.string().required('Select your role'),
+    password: yup.string().min(4, 'Password must be at least 4 characters').required('Password is required'),
+    confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required('Confirm Password is required'),
+  });
 
-    if (!values.email) {
-      errors.email = 'Required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-      errors.email = 'Invalid email address';
+  const handleSubmit = async (values , { resetForm }) => {
+    try {
+      setSubmitting(true);
+      // eslint-disable-next-line no-unused-vars
+      const { confirmPassword, ...dataToSubmit } = values;
+      const response = await API.post('/api/auth/register', dataToSubmit);
+
+      console.log('User registered:', response.data);
+      resetForm();
+    } 
+    catch (error) {
+      console.error('Error registering user:', error);
     }
-
-    if (!values.password) {
-      errors.password = 'Required';
-    } else if (values.password.length < 6) {
-      errors.password = 'Must be 6 characters or more';
+    finally {
+      setSubmitting(false);
     }
-
-    if (values.confirmPassword !== values.password) {
-      errors.confirmPassword = 'Passwords do not match';
-    }
-
-    return errors;
   };
 
   return (
     <div className={styles.registerContainer}>
       <Formik
-        initialValues={{ email: '', password: '', confirmPassword: '' }}
+        initialValues={{ fullname: '', email: '', role: '', password: '', confirmPassword: '' }}
         onSubmit={handleSubmit}
-        validate={handleValidate}
+        validationSchema={YupSchema}
       >
+        {({values , setFieldValue }) => (
         <Form className={styles.registerForm}>
-          <h2 className={styles.title}>Create Account</h2>
-          
-          <div className={styles.formGroup}>
-            <label htmlFor="email">Email</label>
-            <Field type="email" id="email" name="email" required />
-            <ErrorMessage name="email" component="div" className={styles.error} />
+          <h2 className={'text-2xl font-bold mb-4'}>Create Account</h2>
+          {
+            formFields.map(field => (
+              <div className={styles.formGroup} key={field.id}>
+                <label htmlFor={field.id}>{field.label}</label>
+                <Field type={field.type} id={field.id} name={field.id} required={field.required} />
+                <ErrorMessage name={field.id} component="div" className={styles.error} />
+              </div>
+            ))
+          }
+          <div className={styles.roleSelector}>
+            <button type="button" className={`${styles.roleButton} ${values.role === 'tenant' ? styles.active : ''}`}
+                onClick={() => setFieldValue('role', 'tenant')}>
+                I'm a Tenant
+            </button>
+            <button
+              type="button" className={`${styles.roleButton} ${values.role === 'owner' ? styles.active : ''}`}
+              onClick={() => setFieldValue('role', 'owner')}>
+                I'm an Owner
+            </button>
+            <ErrorMessage name="role" component="div" className={styles.error} />
           </div>
-
-          <div className={styles.formGroup}>
-            <label htmlFor="password">Password</label>
-            <Field type="password" id="password" name="password" required />
-            <ErrorMessage name="password" component="div" className={styles.error} />
-          </div>
-
-          <div className={styles.formGroup}>
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <Field type="password" id="confirmPassword" name="confirmPassword" required />
-            <ErrorMessage name="confirmPassword" component="div" className={styles.error} />
-          </div>
-
           <button type="submit" className={styles.submitButton} disabled={isSubmitting}>
             {isSubmitting ? 'Waiting...' : 'Register'}
           </button>
-
           <p className={styles.loginLink}>
             Already have an account? <Link to="/login">Log in</Link>
           </p>
         </Form>
+        )}
       </Formik>
     </div>
   );
